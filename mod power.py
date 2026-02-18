852a989096e1461abde94d02c65c00cdb3419f81
Zuliy <Zuleykaseid19@gmail.com>
2026-02-18 15:59:43 +0300

    Add power calculation and modular exponentiation
    Implement modular exponentiation and basic power calculation.

---
+++ mod power.py
@@ -0,0 +1,10 @@
+# Read input values
+a = int(input())
+b = int(input())
+m = int(input())
+
+# First line: result of pow(a, b)
+print(pow(a, b))
+
+# Second line: result of pow(a, b, m) which is (a^b) % m
+print(pow(a, b, m))
