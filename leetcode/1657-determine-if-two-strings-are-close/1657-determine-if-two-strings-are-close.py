dee006eaba2d75914708582fd3f01933ec3f4dbc
Zuliy <Zuleykaseid19@gmail.com>
2026-03-03 22:53:36 +0300

    Add solution for Determine if Two Strings Are Close

--- leetcode/1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ leetcode/1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -1,10 +1,10 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
-        # Step 1: Length must match
+   
         if len(word1) != len(word2):
             return False
         
-        # Step 2: Count frequency manually
+   
         freq1 = {}
         freq2 = {}
         
@@ -14,11 +14,11 @@ class Solution:
         for ch in word2:
             freq2[ch] = freq2.get(ch, 0) + 1
         
-        # Step 3: Characters must be the same
+        
         if set(freq1.keys()) != set(freq2.keys()):
             return False
         
-        # Step 4: Frequencies must match after sorting
+        
         if sorted(freq1.values()) != sorted(freq2.values()):
             return False
         
