33eb4d791930e327d229073163d32b7326794679
Zuliy <Zuleykaseid19@gmail.com>
2026-03-16 22:33:52 +0300

    Add solution for Partition Labels

---
+++ leetcode/0763-partition-labels/0763-partition-labels.py
@@ -0,0 +1,16 @@
+class Solution:
+    def partitionLabels(self, s: str):
+        last = {c: i for i, c in enumerate(s)}
+        
+        result = []
+        start = 0
+        end = 0
+   
+        for i, c in enumerate(s):
+            end = max(end, last[c])
+            
+            if i == end:
+                result.append(end - start + 1)
+                start = i + 1
+        
+        return result
