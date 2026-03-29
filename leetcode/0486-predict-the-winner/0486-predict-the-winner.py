e4283b41696ccbeca8bccf95c633ca0379e0a413
Zuliy <Zuleykaseid19@gmail.com>
2026-03-29 22:01:47 +0300

    Add solution for Predict the Winner

---
+++ leetcode/0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,15 @@
+class Solution:
+    def predictTheWinner(self, nums):
+        from functools import lru_cache
+        
+        @lru_cache(None)
+        def dp(l, r):
+            if l == r:
+                return nums[l]
+            
+            pick_left = nums[l] - dp(l+1, r)
+            pick_right = nums[r] - dp(l, r-1)
+            
+            return max(pick_left, pick_right)
+        
+        return dp(0, len(nums) - 1) >= 0
